b642a58f559026b1203a66de276009535d3d675a
José Del Castillo González <alu0100956955@ull.edu.es>
2018-11-14 16:16:45 +0000

    C50: Creo el metodo insert_tail y la prueba funciona

--- lib/proyecto_etiqueta/lista.rb
+++ lib/proyecto_etiqueta/lista.rb
@@ -10,4 +10,19 @@ class Lista
         @head, @tail = head,tail
     end
 
+    def insert_tail (nodo)  # Insertar desde la cola  ( este es el que usaremos para practica 7)
+
+        nodo.prev = @tail
+        @tail = nodo
+        if (@head == NIL)
+            @head = nodo
+        end
+        nodo.nest = NIL
+        if (nodo.prev != NIL)
+            nodo.prev.nest = nodo
+        end
+    end
+
+
+
 end
