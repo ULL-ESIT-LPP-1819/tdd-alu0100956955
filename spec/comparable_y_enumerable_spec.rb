9e2e92a6d602446f3bb8072a63cc900a3907cd06
José Del Castillo González <alu0100956955@ull.edu.es>
2018-11-28 10:10:48 +0000

    C70: creación de las pruebas comparativas de la clase antropometrico y falla

--- spec/comparable_y_enumerable_spec.rb
+++ spec/comparable_y_enumerable_spec.rb
@@ -11,19 +11,41 @@ RSpec.describe ProyectoEtiqueta do
 	end
 
 	describe "# Pruebas de los metodos comparables en la clase etiqueta" do
-		it "la etiqueta 1 es menor que la etiqueta 2" do
+		it "la etiqueta 1 tiene menos kcal que la etiqueta 2" do
 			expect(@et1<@et2).to eq(true)
 		end
-		it "la etiqueta 3 es MAYOR que la etiqueta 3"do
+		it "la etiqueta 3 tiene mas Kcal que la etiqueta 3"do
 			expect(@et3>@et2).to eq(true)
 		end
-		it "la etiqueta 2 es menor o igual que la 3" do
+		it "la etiqueta 2 tiene menos o la misma cantidad de Kcal que la 3" do
 			expect(@et2<=@et3).to eq(true)
 		end
-		it "la etiqueta 3 es mayor o igual que la 1" do
+		it "la etiqueta 3 tiene mas o la misma cantidad de Kcal que la 1" do
 			expect(@et3>=@et1).to eq(true)
 		end
+		it "La etiqueta 2 esta entre la 1 y la 3" do
+			expect(@et2.between?(@et1,@et3)).to eq(true)
+		end
 	end
+
+	describe "# Pruebas de los metodos comparables en la clase individuo" do
+		it "El individuo 1 tiene menor IMC que el 2" do
+			expect(@ant1<@ant2).to eq(true)
+		end
+		it "El individuo 3 tiene mayor IMC que el 2" do
+                        expect(@ant3>@ant2).to eq(true)
+                end
+		it "El individuo 2 tiene menor o igual IMC que el 3" do
+                        expect(@ant2<@ant3).to eq(true)
+                end
+                it "El individuo 3 tiene mayor o igual IMC que el 2" do
+                        expect(@ant3>@ant2).to eq(true)
+                end
+		it "El individo 2 se encuentra entre el individuo 1 y 3" do
+			expect(@ant2.between?(@ant1,@ant3)).to eq(true)
+		end
+	end
+
 
 
 end
